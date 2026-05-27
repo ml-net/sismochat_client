87e7e195fb5142eb8070b0f1fa4c19ab1289fbe8
Marco Lupi <marco.lupi@tui.com>
2026-05-27 16:25:48 +0200

    feat: configure vue router with placeholder views

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import router from './router'
 import App from './App.vue'
 import './style.css'
 
 const app = createApp(App)
 
+app.use(router)
 app.use(PrimeVue, {
   theme: {
     preset: Aura
